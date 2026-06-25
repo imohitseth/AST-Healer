6809cde0fe3b14f6d49b7a56cee21b6c8a6d047e
imohitseth <imohitseth@gmail.com>
2026-06-25 13:43:46 +0530

    Fix test failures

--- tests/mock_code.py
+++ tests/mock_code.py
@@ -10,5 +10,7 @@ def get_first_element(lst: list) -> any:
     return lst[1]
 
 def divide_numbers(a: int, b: int) -> float:
-    
+    if b == 0:
+        return 0.0
     return a / b
+#you can remove the if condition while testing so that it automatically detects it and modifies
